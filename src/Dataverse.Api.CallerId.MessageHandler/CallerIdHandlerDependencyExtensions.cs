c23f48b69e181f594a589448912d26186f828907
Novikov Dmitry <sauric@vk.com>
2021-12-09 10:10:02 +0300

    Intit Caller id Message Handler

---
+++ src/Dataverse.Api.CallerId.MessageHandler/CallerIdHandlerDependencyExtensions.cs
@@ -0,0 +1,24 @@
+﻿using PrimeFuncPack;
+using System;
+using System.Net.Http;
+
+namespace GGroupp.Infra;
+
+public static class CallerIdHandlerDependencyExtensions
+{
+    public static Dependency<DelegatingHandler> UseCallerId<THandler>(
+        this Dependency<THandler> dependency, 
+        Func<IServiceProvider, IAsyncValueFunc<Guid>> callerIdProviderResolver)
+        where THandler : HttpMessageHandler
+        =>
+        InnerUseCallerId(
+            dependency ?? throw new ArgumentNullException(nameof(dependency)),
+            callerIdProviderResolver ?? throw new ArgumentNullException(nameof(callerIdProviderResolver)));
+
+    private static Dependency<DelegatingHandler> InnerUseCallerId<THandler>(
+        Dependency<THandler> dependency, 
+        Func<IServiceProvider, IAsyncValueFunc<Guid>> callerIdProviderResolver)
+        where THandler : HttpMessageHandler
+        =>
+        dependency.With(callerIdProviderResolver).Fold<DelegatingHandler>(CallerIdDelegatingHandler.Create);
+}
---
+++ src/Dataverse.Api.CallerId.MessageHandler/HttpMessageHandler.CallerId/CallerIdDelegatingHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+
+namespace GGroupp.Infra;
+
+internal sealed partial class CallerIdDelegatingHandler : DelegatingHandler
+{
+    public static CallerIdDelegatingHandler Create(HttpMessageHandler innerHandler, IAsyncValueFunc<Guid> callerIdProvider)
+        => 
+        new(
+            innerHandler ?? throw new ArgumentNullException(nameof(innerHandler)),
+            callerIdProvider ?? throw new ArgumentNullException(nameof(callerIdProvider)));
+
+    private readonly IAsyncValueFunc<Guid> callerIdProvider;
+
+    private const string MSCRMCallerID = "MSCRMCallerID";
+
+    private CallerIdDelegatingHandler(HttpMessageHandler innerHandler, IAsyncValueFunc<Guid> callerIdProvider) : base(innerHandler)
+        =>
+        this.callerIdProvider = callerIdProvider;
+}
