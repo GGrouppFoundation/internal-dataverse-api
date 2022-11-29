f585939c810c774d0c3299f6969391151217d6af
pmosk <pm@ggroupp.ru>
2022-11-29 22:11:02 +0400

    Use conditional build

--- src/api-core/Api.Core.EntityKey/DataverseAlternateKey.cs
+++ src/api-core/Api.Core.EntityKey/DataverseAlternateKey.cs
@@ -7,14 +7,26 @@ namespace GGroupp.Infra;
 public sealed record class DataverseAlternateKey : IDataverseEntityKey
 {
     public DataverseAlternateKey(IReadOnlyCollection<KeyValuePair<string, string>> idArguments)
-    {
-        var args = idArguments?.Where(kv => string.IsNullOrEmpty(kv.Value) is false);
-        Value = BuildValue(args ?? Enumerable.Empty<KeyValuePair<string, string>>());
-    }
+        =>
+        Value = BuildValue(idArguments);
 
     public string Value { get; }
 
-    private static string BuildValue(IEnumerable<KeyValuePair<string, string>> args)
-        =>
-        string.Join(',', args.Select(kv => WebUtility.UrlEncode($"{kv.Key}={kv.Value}")));
+    private static string BuildValue(IReadOnlyCollection<KeyValuePair<string, string>>? idArguments)
+    {
+        if (idArguments?.Count is not > 0)
+        {
+            return string.Empty;
+        }
+
+        return string.Join(',', idArguments.Where(IsValueNotEmpty).Select(BuildValueItem));
+
+        static bool IsValueNotEmpty(KeyValuePair<string, string> kv)
+            =>
+            string.IsNullOrEmpty(kv.Value) is false;
+
+        static string BuildValueItem(KeyValuePair<string, string> kv)
+            =>
+            WebUtility.UrlEncode($"{kv.Key}={kv.Value}");
+    }
 }
